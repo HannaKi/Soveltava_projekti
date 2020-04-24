c4b706124b99b3e0ce4b62d84b98cd1d85025c21
Hanna Kitti <hrkitt@utu.fi>
2020-04-24 20:29:02 +0300

    functional plot

--- app.R
+++ app.R
@@ -4,73 +4,10 @@ if(!require(readr)) install.packages("readr")
 if(!require(stringr)) install.packages("stringr")
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(ggplot2)) install.packages("ggplot2")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(tidyr)) install.packages("tidyr")
 
-urlfile="https://raw.githubusercontent.com/eparker12/nCoV_tracker/master/input_data/jhu_data.csv"
-# mydata <- readr::read_csv(url(urlfile))
-
-readUrl <- function(url) {
-  out <- tryCatch(
-    {
-      # Just to highlight: if you want to use more than one 
-      # R expression in the "try" part then you'll have to 
-      # use curly brackets.
-      # 'tryCatch()' will return the last evaluated expression 
-      # in case the "try" part was completed successfully
-      
-      message("This is the 'try' part")
-      
-      readr::read_csv(url(url))
-      # The return value of `readLines()` is the actual value 
-      # that will be returned in case there is no condition 
-      # (e.g. warning or error). 
-      # You don't need to state the return value via `return()` as code 
-      # in the "try" part is not wrapped insided a function (unlike that
-      # for the condition handlers for warnings and error below)
-      
-    },
-    error=function(cond) {
-      message(paste("URL does not seem to exist:", url))
-      message("Here's the original error message:")
-      message(cond)
-      # Choose a return value in case of error
-      return(NA)
-      # TODO: in case of error, read latest downlaoded data
-    },
-    warning=function(cond) {
-      message(paste("URL caused a warning:", url))
-      message("Here's the original warning message:")
-      message(cond)
-      # Choose a return value in case of warning
-      return(readr::read_csv(url(url)))
-    },
-    finally={
-      # NOTE:
-      # Here goes everything that should be executed at the end,
-      # regardless of success or error.
-      # If you want more than one expression to be executed, then you 
-      # need to wrap them in curly brackets ({...}); otherwise you could
-      # just have written 'finally=<expression>' 
-      message(paste("Processed URL:", url))
-      message("Data red in")
-    }
-  )    
-  return(out)
-}
-
-mydata <- readUrl(urlfile)
-
-# Select columns using partial match
-
-# first use regex to form binary vector. Regex details: https://stringr.tidyverse.org/articles/regular-expressions.html
-# nordics <- str_detect(colnames(mydata), "Date|Finland.|Sweden.|Iceland.|Norway.|Denmark.")
-fin <- str_detect(colnames(mydata), "Date|Finland.")
-if(is_empty(fin)) { stop(paste0("Error: Data for Finland not found")) }
-
-# then use gained binary vector to select desired columns from the data
-# nordics <- mydata[ , nordics,  drop=FALSE]
-fin <- mydata[ , fin,  drop=FALSE]
-colnames(fin) <- c("Date", "Cases", "Deaths", "Recovered")
-
+library(plotly)
 palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
           "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
@@ -91,39 +28,92 @@ ui <- fluidPage(
     ),
   
   # Sidebar - input
+    
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(fin)),
-    checkboxGroupInput('ycol', 'Y Variable', names(fin),
-                       selected = names(fin)[[2]])
+    selectizeInput("metrics",
+                   "Select case:",
+                   choices = c("Cases", 
+                               "Deaths",
+                               "Recovered"),
+                   selected = "Cases",
+                   multiple = TRUE
+    )
   ),
+  
   
   # Plot - output
   mainPanel(
-    plotOutput('lineplot2'),
-    plotOutput('hist'),
-    
-    a("Data source",
-      href = "http://www.rstudio.com/
-      products/shiny/shiny-user-showcase/")
+    plotOutput('plot'),
+  
+  # Footer
+  a("Data source",
+    href = "http://www.rstudio.com/
+    products/shiny/shiny-user-showcase/")
   )
   )
 
 server <- function(input, output) {
   
-  selectedData <- reactive({
-    # select all rows and columns specified in ui
-    fin[, c(input$xcol, input$ycol)]
-  })
+  urlfile="https://raw.githubusercontent.com/eparker12/nCoV_tracker/master/input_data/jhu_data.csv"
 
-  output$lineplot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    title <- "jokin otsikko"
-    lines(selectedData())
-  })
+  readUrl <- function(url) {
+    out <- tryCatch(
+      {
+        message("Retrieving data")
+        readr::read_csv(url(url))
+      },
+      error=function(cond) {
+        message(paste("URL does not seem to exist:", url))
+        message("Here's the original error message:")
+        message(cond)
+        # Return value in case of error
+        return(NA)
+        # TODO: in case of error, read latest downloaded data
+      },
+      warning=function(cond) {
+        message(paste("URL caused a warning:", url))
+        message("Here's the original warning message:")
+        message(cond)
+        # Return value in case of warning
+        # Since the data causes error we have to accept it!
+        return(readr::read_csv(url(url)))
+      },
+      finally={
+        message(paste("Processed URL:", url))
+        message("Data red in")
+      }
+    )    
+    return(out)
+  }
   
-  output$hist <- renderPlot({
-    title <- "100 random normal values"
-    hist(rnorm(100), main = title)
+  mydata <- readUrl(urlfile)
+  
+  # Select columns using partial match
+  # first use regex to form binary vector. Regex details: https://stringr.tidyverse.org/articles/regular-expressions.html
+  nordics <- str_detect(colnames(mydata), "Date|Finland.|Sweden.|Iceland.|Norway.|Denmark.")
+  fin <- str_detect(colnames(mydata), "Date|Finland.")
+  if(is_empty(fin)) { stop(paste0("Error: Data for Finland not found")) }
+  
+  # then use gained binary vector to select desired columns from the data
+  nordics <- mydata[ , nordics,  drop=FALSE]
+  fin <- mydata[ , fin,  drop=FALSE]
+  colnames(fin) <- c("Date", "Cases", "Deaths", "Recovered")
+  # from wide to long format
+  # fin <- gather(fin, condition, nums, Confirmed:Recovered)
+  
+  
+  # https://stackoverflow.com/questions/45286622/plotting-multiple-lines-on-a-single-graph-using-shiny-and-ggplot2
+  output$plot = renderPlot({
+    #plot.data <- fin
+    fin <- melt(fin, id.vars = 'Date')
+    #not sure if input$cnt is a list or a vector
+    #may need to manipulate that before passing
+    vals <- unlist(input$metrics, use.names=FALSE)
+    fin <- fin[fin$variable %in% vals, ]
+    ggplot(fin) +
+      geom_line(mapping = aes(x = Date, y = value, colour = variable)) + 
+      labs (x = "Years", y = "Nights spent per 1000", title = "Tourism") + 
+      scale_colour_discrete(name = "Country")
   })
 }
 
--- helpers.R
+++ helpers.R
@@ -4,6 +4,7 @@ if(!require(readr)) install.packages("readr")
 if(!require(stringr)) install.packages("stringr")
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(ggplot2)) install.packages("ggplot2")
+library(reshape2)
 
 # Download data 
 
@@ -64,14 +65,22 @@ mydata <- readUrl(urlfile)
 # Select columns using partial match
 
 # first use regex to form binary vector. Regex details: https://stringr.tidyverse.org/articles/regular-expressions.html
-# nordics <- str_detect(colnames(mydata), "Date|Finland.|Sweden.|Iceland.|Norway.|Denmark.")
+nordics <- str_detect(colnames(mydata), "Date|Finland.|Sweden.|Iceland.|Norway.|Denmark.")
 fin <- str_detect(colnames(mydata), "Date|Finland.")
 if(is_empty(fin)) { stop(paste0("Error: Data for Finland not found")) }
 
 # then use gained binary vector to select desired columns from the data
-# nordics <- mydata[ , nordics,  drop=FALSE]
+nordics <- mydata[ , nordics,  drop=FALSE]
 fin <- mydata[ , fin,  drop=FALSE]
 colnames(fin) <- c("Date", "Cases", "Deaths", "Recovered")
 
+tes <- gather(fin, condition, nums, Cases:Recovered)
+tes2 <- melt(fin, id.vars = 'Date')
+
 # ggplot(fin, aes(x=fin$Date, y=fin$Cases)) +
 #   geom_line()
+
+p <- ggplot(data = fin,
+            mapping = aes(x = Date,
+                          y = Cases))
+p + geom_line(aes())

7be919a38cc24c3a27ba5b58c36bf32bf0196b29
Hanna Kitti <hrkitt@utu.fi>
2020-04-26 18:01:29 +0300

    test fixed

--- R/readUrl.R
+++ R/readUrl.R
@@ -5,31 +5,38 @@
 #'
 #' @return csv table as a dataframe
 readUrl <- function(url) {
-  out <- tryCatch(
-    {
-      message("Retrieving data")
-      readr::read_csv(url(url))
-    },
-    error=function(cond) {
-      message(paste("URL does not seem to exist:", url))
-      message("Here's the original error message:")
-      message(cond)
-      # Return value in case of error
-      return(NA)
-      # TODO: in case of error, read latest downloaded data
-    },
-    warning=function(cond) {
-      message(paste("URL caused a warning:", url))
-      message("Here's the original warning message:")
-      message(cond)
-      # Return value in case of warning
-      # Since the data causes error we have to accept it!
-      return(readr::read_csv(url(url)))
-    },
-    finally={
-      message(paste("Processed URL:", url))
-      message("Data red in")
-    }
-  )
-  return(out)
+
+  readr::read_csv(url(url))
 }
+
+
+
+# readUrl <- function(url) {
+#   out <- tryCatch(
+#     {
+#       message("Retrieving data")
+#       readr::read_csv(url(url))
+#     },
+#     error=function(cond) {
+#       message(paste("URL does not seem to exist:", url))
+#       message("Here's the original error message:")
+#       message(cond)
+#       # Return value in case of error
+#       return(NA)
+#       # TODO: in case of error, read latest downloaded data
+#     },
+#     warning=function(cond) {
+#       message(paste("URL caused a warning:", url))
+#       message("Here's the original warning message:")
+#       message(cond)
+#       # Return value in case of warning
+#       # Since the data causes error we have to accept it!
+#       return(readr::read_csv(url(url)))
+#     },
+#     finally={
+#       message(paste("Processed URL:", url))
+#       message("Data red in")
+#     }
+#   )
+#   return(out)
+# }
--- tests/testthat/test-readUrl.R
+++ tests/testthat/test-readUrl.R
@@ -1,16 +1,13 @@
 context("readUrl")
 
-# toy test made for the purpose of testing
-
-testUrl = "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv" %>%
+testUrl <-  "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv" %>%
   soveltavatyo::readUrl()
-  testobject <- readUrl(testUrl)
+
 
 test_that("readUrl returns output of expected class", {
 
   expect_true(
-    #class(readUrl(testUrl))[4] == "data.frame"
-    class(testobject) == "data.frame"
+    class(testUrl)[4] == "data.frame"
 
   )
 })

9bbb77072bf5c6ac04936507cbb85ee0fca20cb6
Hanna Kitti <hrkitt@utu.fi>
2020-04-26 18:44:02 +0300

    tests passed

--- tests/testthat/test-readUrl.R
+++ tests/testthat/test-readUrl.R
@@ -4,13 +4,11 @@ context("readUrl")
 
 testUrl = "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv" %>%
   soveltavatyo::readUrl()
-  testobject <- readUrl(testUrl)
+#  testobject <- readUrl(testUrl)
 
 test_that("readUrl returns output of expected class", {
 
   expect_true(
-    #class(readUrl(testUrl))[4] == "data.frame"
-    class(testobject) == "data.frame"
-
+    class(testUrl)[4] == "data.frame"
   )
 })
